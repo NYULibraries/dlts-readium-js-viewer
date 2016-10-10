fcf1cb1b690f9d9d9d5037c037e421917a610c06
David <da70@nyu.edu>
2016-10-10 14:16:39 -0400

    Rename tests/ -> test/

--- test/browser/wdio.conf.js
+++ test/browser/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        'tests/browser/**/*.js'
+        'test/browser/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
