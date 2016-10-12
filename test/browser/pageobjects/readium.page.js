630466c040ab610343e07b6dc48049eeabaa038c
David <da70@nyu.edu>
2016-10-12 17:56:51 -0400

    readium.page.js: add style members

--- test/browser/pageobjects/readium.page.js
+++ test/browser/pageobjects/readium.page.js
@@ -1,7 +1,25 @@
 let Page = require('./page');
 
 let ReadiumPage = Object.create( Page, {
-    navbar: { get: function() { return browser.element( '#app-navbar' ); } },
+    navbar: { get:
+        function() {
+            let element = browser.element( '#app-navbar' );
+            let backgroundColor = element.getCssProperty( 'background-color' ).parsed.hex;
+            let boxShadow       = element.getCssProperty( 'box-shadow' ).value;
+            let borderRadius    = element.getCssProperty( 'border-radius' ).value;
+            let minHeight       = element.getCssProperty( 'min-height' ).value;
+            let marginBottom    = element.getCssProperty( 'margin-bottom' ).value;
+
+            return {
+                element,
+                backgroundColor,
+                boxShadow,
+                borderRadius,
+                minHeight,
+                marginBottom,
+            }
+        }
+    },
 
     open: { value: function( path ) {
         Page.open.call( this, path );
