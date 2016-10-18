4958a1b9336bd985acb335ad72d8f6070285b70c
David <da70@nyu.edu>
2016-10-18 12:43:18 -0400

    wdio.main.conf.js specs configuration: change tests.js to *.js

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        'test/browser/tests/tests.js'
+        'test/browser/tests/*.js'
     ],
     // Patterns to exclude.
     exclude: [
