f64ea3040e11247d5c648be45435108427571446
David <da70@nyu.edu>
2016-10-18 15:41:08 -0400

    wdio.main.conf.js: enable Firefox capabilities

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -49,14 +49,14 @@ exports.config = {
             //
             browserName: 'chrome'
         },
-        // {
-        //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        //     // grid with only 5 firefox instance available you can make sure that not more than
-        //     // 5 instance gets started at a time.
-        //     maxInstances: 5,
-        //     //
-        //     browserName: 'firefox'
-        // },
+        {
+            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+            // grid with only 5 firefox instance available you can make sure that not more than
+            // 5 instance gets started at a time.
+            maxInstances: 2,
+            //
+            browserName: 'firefox'
+        },
     ],
     //
     // ===================
