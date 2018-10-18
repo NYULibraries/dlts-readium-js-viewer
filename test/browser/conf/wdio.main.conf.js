71b24d79606e6692f3f245899a62e0b89c69c184
David <da70@nyu.edu>
2018-10-18 12:15:08 -0400

    tests: add selenium standalone service and chromedriver service

--- test/browser/conf/wdio.main.conf.js
+++ test/browser/conf/wdio.main.conf.js
@@ -1,7 +1,7 @@
 "use strict";
 
 exports.config = {
-    
+
     //
     // ==================
     // Specify Test Files
@@ -163,7 +163,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],//
+    services: ['selenium-standalone', 'chromedriver'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
